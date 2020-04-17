03ffb059151094a5f26dfa07075052e6c23f3652
WerewolfDevLearn <werewolfdevlearn@gmail.com>
2020-04-17 21:19:55 +0300

    add task-4.js and html files for each task

---
+++ script/task-4.js
@@ -0,0 +1,20 @@
+let credits = 23580;
+const pricePerDroid = 3000;
+let totalPrice = 0;
+let numderOfDrones = prompt('Какое количество дронов Вы хотите перобрести?');
+if (numderOfDrones === null) {
+  console.log('Отменено пользователем!');
+} else {
+  totalPrice = Number(numderOfDrones) * pricePerDroid;
+  if (totalPrice > credits) {
+    console.log('Недостаточно средств на счету!');
+  } else {
+    credits -= totalPrice;
+    alert(
+      `Вы купили ${numderOfDrones} дроидов, на счету осталось ${credits} кредитов.`,
+    );
+  }
+} 
+else if(numderOfDrones==='0'){
+    alert(`Вы ничего не купили, на счету ${credits} кредитов`)
+}
